7c205f9de5dc76c4becacfe60dc27ddf10db8edd
kimuky <xogus559@gmail.com>
2024-11-18 13:04:57 +0900

    fix : 유저 PK 타입 수정

--- src/main/java/com/example/idontknowspring/repository/UserRepository.java
+++ src/main/java/com/example/idontknowspring/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.example.idontknowspring.repository;
 import com.example.idontknowspring.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
 }
